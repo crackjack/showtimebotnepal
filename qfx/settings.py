babc5381237aa5c0a5505e8aaca51c0f048f2ee9
Nitesh Rijal <rijal.it@gmail.com>
2016-12-31 14:17:05 +0545

    added debug true for production

--- qfx/settings.py
+++ qfx/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd#$no$ilr^%2yi7^wyi!2c_nd!n-xdh9nj&f_$t^0@dxu@wie8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
