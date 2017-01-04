1ed3bafe57d98416868648dcfaf52c3791e40070
Nitesh Rijal <rijal.it@gmail.com>
2017-01-04 17:39:10 +0545

    add new templates for now, up and movies

--- bot/views.py
+++ bot/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
 import json
 import requests
+from messengerbot import MessengerClient, messages, attachments, templates, elements
 from movies.models import Movie
 from showtimes.models import Showtime
 
@@ -66,45 +67,58 @@ def show_movie_list(fbid, _data):
 
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 
+def show_welcome_message():
+    btn_now_playing = elements.PostbackButton(
+       title='Now Playing',
+       payload='now'
+    )
+
+    btn_up_coming = elements.PostbackButton(
+       title='Upcoming',
+       payload='up'
+    )
+
+    template_btn = templates.ButtonTemplate(
+       text='What movies are you looking for?',
+       buttons=[
+           btn_now_playing, btn_up_coming
+       ]
+    )
+
+    attachment = attachments.TemplateAttachment(template=template_btn)
+
+    message = messages.Message(attachment=attachment)
+    request = messages.MessageRequest(recipient, message)
+    messenger.send(request)
+
+def show_movies(recipient, _data):
+    buttons = []
+
+    for d in _data:
+        btn = elements.PostbackButton(
+                title=str(d.name),
+                payload=str(d.name)
+        )
+
+        buttons.append(d)
+
+    template_btn = templates.ButtonTemplate(
+       text='Which Movie?',
+       buttons=buttons
+    )
+
+    attachment = attachments.TemplateAttachment(template=template_btn)
+
+    message = messages.Message(attachment=attachment)
+    request = messages.MessageRequest(recipient, message)
+    messenger.send(request)
+
 def show_movie_detail(fbid, _data):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % _PAGE_TOKEN
     msg_dict = dict()
     msg_dict['recipient'] = {"id":fbid}
 
     msg_dict['message'] = dict()
-
-  #   sss = json.dumps({
-  # "recipient":{
-  #   "id":fbid
-  # }, "message":{
-  #   "attachment":{
-  #     "type":"template",
-  #     "payload":{
-  #       "template_type":"generic",
-  #       "elements": [{
-  #           "title": "Yo...",
-  #           "image_url":"https://petersfancybrownhats.com/company_image.png",
-  #           "subtitle":"Wve got the right hat for everyone.",
-  #           "buttons":[
-  #             {
-  #               "type":"web_url",
-  #               "url":"aa",
-  #               "title":"Watch Trailer"
-  #             },
-  #             {
-  #               "type":"web_url",
-  #               "url":"bb",
-  #               "title":"Watch Trailer"
-  #             },
-  #           ]
-  #       }],
-  #     }
-  #   }
-  # }
-
-# })
-
-    # print sss
 
     qr = []
     for d in _data:
@@ -143,19 +157,24 @@ class BotView(generic.View):
         movies_object = Movie.objects
         showtime_object = Showtime.objects
         list_movies = [str(mv.name).lower() for mv in movies_object.all()]
-        _type = 'quick'
-        _data = 'Sorry, I cannot handle the request.'
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check to make sure the received call is a message call
                 # This might be delivery, optin, postback for other events
                 if 'message' in message:
                     fb_id = message['sender']['id']
+                    messenger = MessengerClient(access_token=_PAGE_TOKEN)
+                    recipient = messages.Recipient(recipient_id=fb_id)
                     # Print the message to the terminal
                     kw = message.get('message')['text'].lower() if 'text' in message.get('message') else None
-                    if kw == 'now':
+                    if kw == 'yo':
+                        show_welcome_message()
+                    elif kw == 'now':
                         _data = movies_object.filter(status='NP')
-                        show_movie_list(message['sender']['id'], _data)
+                        show_movies(recipient, _data)
+                    elif kw == 'up':
+                        _data = movies_object.filter(status='UP')
+                        show_movies(recipient, _data)
                     elif kw in list_movies:
                         try:
                             mv = movies_object.get(name__iexact=kw)
@@ -163,58 +182,6 @@ class BotView(generic.View):
                             show_movie_detail(fb_id, _data)
                         except Movie.DoesNotExist:
                             show_text_message(fb_id, _data)
-                    elif kw == 'one':
-                        from pymessenger.bot import Bot
-                        from pymessenger import Element
-                        bot = Bot(_PAGE_TOKEN)
-                        elements = []
-                        element = Element(title="test", image_url="", subtitle="subtitle", item_url="http://arsenal.com")
-                        elements.append(element)
-                        bot.send_generic_message(fb_id, elements)
-                    # elif kw == 'two':
-                    #     from message import SendMessage
-                    #     message = SendMessage(fb_id)
-                    #     project_styletrip = GenericElement('Chinese 111',
-                    #                                        'do not say',
-                    #                                        'styletrip/introduction.jpeg', [
-                    #                                            ActionButton(ButtonType.POSTBACK,
-                    #                                                         'test',
-                    #                                                         payload=Payload.INTRODUCE.name)
-                    #                                        ])
-                    #     project_movie_lol = GenericElement('Movie lol App',
-                    #                                        'some pp',
-                    #                                        'movielol/introduction.jpeg', [
-                    #                                            ActionButton(ButtonType.WEB_URL,
-                    #                                                         'GitHub',
-                    #                                                         url='https://github.com/enginebai/Movie-lol-android')
-                    #                                        ])
-                    #     message.build_generic_message([project_styletrip, project_movie_lol]).send_message()
-                    elif kw == 'three':
-                        from messengerbot import MessengerClient, messages, attachments, templates, elements
-                        messenger = MessengerClient(access_token=_PAGE_TOKEN)
-                        recipient = messages.Recipient(recipient_id=fb_id)
-                        # Send button template
-                        web_button = elements.WebUrlButton(
-                           title='Show website',
-                           url='https://petersapparel.parseapp.com'
-                        )
-                        postback_button = elements.PostbackButton(
-                           title='Start chatting',
-                           payload='USER_DEFINED_PAYLOAD'
-                        )
-                        template = templates.ButtonTemplate(
-                           text='What do you want to do next?',
-                           buttons=[
-                               web_button, postback_button
-                           ]
-                        )
-                        attachment = attachments.TemplateAttachment(template=template)
-
-                        message = messages.Message(attachment=attachment)
-                        request = messages.MessageRequest(recipient, message)
-                        messenger.send(request)
                     else:
                         show_text_message(fb_id, _data)
-
-                    # post_facebook_message(message['sender']['id'], _data, _type)
         return HttpResponse()
